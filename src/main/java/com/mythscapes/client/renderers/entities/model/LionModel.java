2e5622aa4e56420887c0633c2aefd255224ca57f
Sarinsa <mikkemusbruker@gmail.com>
2020-05-01 22:06:09 +0200

    Fixed limb rotation for the lion.

--- src/main/java/com/mythscapes/client/renderers/entities/model/LionModel.java
+++ src/main/java/com/mythscapes/client/renderers/entities/model/LionModel.java
@@ -20,14 +20,15 @@ public class LionModel<T extends LionEntity> extends AgeableModel<T> {
     public ModelRenderer body;
     public ModelRenderer head;
     public ModelRenderer tail;
-    public ModelRenderer frontleftleg;
-    public ModelRenderer frontrightleg;
-    public ModelRenderer backleftleg;
-    public ModelRenderer backrightleg;
+    public ModelRenderer frontLeftLeg;
+    public ModelRenderer frontRightLeg;
+    public ModelRenderer backLeftLeg;
+    public ModelRenderer backRightLeg;
     public ModelRenderer muzzle;
     public ModelRenderer mane;
     public ModelRenderer earright;
     public ModelRenderer earleft;
+
 
     public LionModel() {
         this.textureWidth = 96;
@@ -42,18 +43,18 @@ public class LionModel<T extends LionEntity> extends AgeableModel<T> {
         this.tail.addBox(0.0F, 0.0F, 0.0F, 2, 2, 14, 0.0F);
         this.setRotateAngle(tail, -0.8850564636863244F, 0.0F, 0.0F);
 
-        this.backrightleg = new ModelRenderer(this, 0, 0);
-        this.backrightleg.setRotationPoint(6.0F, 10.0F, 23.0F);
-        this.backrightleg.addBox(0.0F, 0.0F, -2.0F, 4, 10, 4, 0.0F);
+        this.backRightLeg = new ModelRenderer(this, 0, 0);
+        this.backRightLeg.setRotationPoint(6.0F, 10.0F, 23.0F);
+        this.backRightLeg.addBox(0.0F, 0.0F, -2.0F, 4, 10, 4, 0.0F);
 
         this.earleft = new ModelRenderer(this, 0, 14);
         this.earleft.setRotationPoint(7.0F, -2.0F, 2.0F);
         this.earleft.addBox(-5.0F, -7.0F, -10.0F, 3, 2, 2, 0.0F);
 
-        this.backleftleg = new ModelRenderer(this, 0, 0);
-        this.backleftleg.mirror = true;
-        this.backleftleg.setRotationPoint(0.0F, 10.0F, 23.0F);
-        this.backleftleg.addBox(0.0F, 0.0F, -2.0F, 4, 10, 4, 0.0F);
+        this.backLeftLeg = new ModelRenderer(this, 0, 0);
+        this.backLeftLeg.mirror = true;
+        this.backLeftLeg.setRotationPoint(0.0F, 10.0F, 23.0F);
+        this.backLeftLeg.addBox(0.0F, 0.0F, -2.0F, 4, 10, 4, 0.0F);
 
         this.earright = new ModelRenderer(this, 0, 14);
         this.earright.mirror = true;
@@ -64,32 +65,41 @@ public class LionModel<T extends LionEntity> extends AgeableModel<T> {
         this.body.setRotationPoint(-5.0F, 4.0F, -14.0F);
         this.body.addBox(0.0F, 0.0F, 0.0F, 10, 10, 25, 0.0F);
 
-        this.frontrightleg = new ModelRenderer(this, 0, 0);
-        this.frontrightleg.mirror = true;
-        this.frontrightleg.setRotationPoint(0.0F, 10.0F, 3.0F);
-        this.frontrightleg.addBox(0.0F, 0.0F, -2.0F, 4, 10, 4, 0.0F);
+        this.frontRightLeg = new ModelRenderer(this, 0, 0);
+        this.frontRightLeg.mirror = true;
+        this.frontRightLeg.setRotationPoint(0.0F, 10.0F, 3.0F);
+        this.frontRightLeg.addBox(0.0F, 0.0F, -2.0F, 4, 10, 4, 0.0F);
 
         this.muzzle = new ModelRenderer(this, 45, 18);
         this.muzzle.setRotationPoint(2.0F, 5.0F, -2.0F);
         this.muzzle.addBox(-5.0F, -7.0F, -9.1F, 6, 5, 2, 0.0F);
 
-        this.frontleftleg = new ModelRenderer(this, 0, 0);
-        this.frontleftleg.setRotationPoint(6.0F, 10.0F, 3.0F);
-        this.frontleftleg.addBox(0.0F, 0.0F, -2.0F, 4, 10, 4, 0.0F);
+        this.frontLeftLeg = new ModelRenderer(this, 0, 0);
+        this.frontLeftLeg.setRotationPoint(6.0F, 10.0F, 3.0F);
+        this.frontLeftLeg.addBox(0.0F, 0.0F, -2.0F, 4, 10, 4, 0.0F);
 
         this.mane = new ModelRenderer(this, 0, 35);
         this.mane.setRotationPoint(-7.0F, -4.0F, 3.0F);
         this.mane.addBox(0.0F, -7.0F, -10.3F, 14, 17, 9, 0.0F);
 
         this.body.addChild(this.tail);
-        this.body.addChild(this.backrightleg);
+        this.body.addChild(this.backRightLeg);
         this.head.addChild(this.earleft);
-        this.body.addChild(this.backleftleg);
+        this.body.addChild(this.backLeftLeg);
         this.head.addChild(this.earright);
-        this.body.addChild(this.frontrightleg);
+        this.body.addChild(this.frontRightLeg);
         this.head.addChild(this.muzzle);
-        this.body.addChild(this.frontleftleg);
+        this.body.addChild(this.frontLeftLeg);
         this.head.addChild(this.mane);
+
+        --this.backRightLeg.rotationPointX;
+        ++this.backLeftLeg.rotationPointX;
+        this.backRightLeg.rotationPointZ += 0.0F;
+        this.backLeftLeg.rotationPointZ += 0.0F;
+        --this.frontRightLeg.rotationPointX;
+        ++this.frontLeftLeg.rotationPointX;
+        --this.frontRightLeg.rotationPointZ;
+        --this.frontLeftLeg.rotationPointZ;
     }
 
     @Override
@@ -104,10 +114,10 @@ public class LionModel<T extends LionEntity> extends AgeableModel<T> {
     public void setRotationAngles(T entity, float limbSwing, float limbSwingAmount, float ageInTicks, float headYaw, float headPitch) {
         this.head.rotateAngleX = headPitch * ((float)Math.PI / 180F);
         this.head.rotateAngleY = headYaw * ((float)Math.PI / 180F);
-        this.backrightleg.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F) * 1.4F * limbSwingAmount;
-        this.backleftleg.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F + (float)Math.PI) * 1.4F * limbSwingAmount;
-        this.frontrightleg.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F + (float)Math.PI) * 1.4F * limbSwingAmount;
-        this.frontleftleg.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F) * 1.4F * limbSwingAmount;
+        this.backRightLeg.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F) * 1.4F * limbSwingAmount;
+        this.backLeftLeg.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F + (float)Math.PI) * 1.4F * limbSwingAmount;
+        this.frontRightLeg.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F + (float)Math.PI) * 1.4F * limbSwingAmount;
+        this.frontLeftLeg.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F) * 1.4F * limbSwingAmount;
     }
 
     @Override

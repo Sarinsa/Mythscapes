7da7533f3288d020d190fb8e4b12607f078fe119
Sarinsa <mikkemusbruker@gmail.com>
2020-11-05 22:34:05 +0100

    Fixed an issue with StaticCottonPoofParticle sometimes creating a strange line pattern of particles.

--- src/main/java/com/mythscapes/client/particles/StaticCottonPoofParticle.java
+++ src/main/java/com/mythscapes/client/particles/StaticCottonPoofParticle.java
@@ -6,32 +6,21 @@ import net.minecraft.client.particle.MetaParticle;
 import net.minecraft.client.particle.Particle;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.particles.BasicParticleType;
-import net.minecraft.world.World;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
-import org.jetbrains.annotations.NotNull;
 
 public class StaticCottonPoofParticle extends MetaParticle {
 
     private int timeSinceStart;
     private final int maxTime = 5;
 
-    private StaticCottonPoofParticle(ClientWorld world, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed) {
+    private StaticCottonPoofParticle(ClientWorld world, double x, double y, double z) {
         super(world, x, y, z);
-        this.motionX = xSpeed;
-        this.motionY = ySpeed;
-        this.motionZ = zSpeed;
-    }
-
-    private double getVelocityMultiplier() {
-        return ((double)this.timeSinceStart / (double)this.maxTime);
     }
 
     public void tick() {
         for(int i = 0; i < 8; ++i) {
-            double xSpeed = this.motionX;
-            double ySpeed = this.motionY / 2;
-            double zSpeed = this.motionZ;
+            double xSpeed = rand.nextGaussian();
+            double ySpeed = rand.nextGaussian() / 2;
+            double zSpeed = rand.nextGaussian();
             this.world.addParticle(MythParticles.STATIC_COTTON.get(), this.posX, this.posY, this.posZ, xSpeed, ySpeed, zSpeed);
         }
         ++this.timeSinceStart;
@@ -43,7 +32,7 @@ public class StaticCottonPoofParticle extends MetaParticle {
     public static class Factory implements IParticleFactory<BasicParticleType> {
 
         public Particle makeParticle(BasicParticleType type, ClientWorld world, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed) {
-            return new StaticCottonPoofParticle(world, x, y, z, xSpeed, ySpeed, zSpeed);
+            return new StaticCottonPoofParticle(world, x, y, z);
         }
     }
 }

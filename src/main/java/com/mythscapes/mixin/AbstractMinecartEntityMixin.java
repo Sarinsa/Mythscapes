e56d06bc7ab6c29d8f9d3ef3d3af38252d08283a
Sarinsa <mikkemusbruker@gmail.com>
2020-11-05 18:43:36 +0100

    Continuing work on minecart mixin and entity swimming mixin.

--- src/main/java/com/mythscapes/mixin/AbstractMinecartEntityMixin.java
+++ src/main/java/com/mythscapes/mixin/AbstractMinecartEntityMixin.java
@@ -11,12 +11,13 @@ import net.minecraft.particles.ParticleTypes;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.SoundEvents;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.common.extensions.IForgeEntityMinecart;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(AbstractMinecartEntity.class)
@@ -26,23 +27,24 @@ public abstract class AbstractMinecartEntityMixin extends Entity implements IFor
         super(entityTypeIn, worldIn);
     }
 
-    //@Inject(method = "moveMinecartOnRail", at = @At("HEAD"), cancellable = true)
-    public void onMoveMinecartOnRail(BlockPos pos, CallbackInfo callbackInfo) {
-        BlockState state = this.world.getBlockState(pos);
-
+    @Inject(method = "moveAlongTrack", at = @At("HEAD"), cancellable = true)
+    public void onMoveMinecartOnRail(BlockPos pos, BlockState state, CallbackInfo callbackInfo) {
         if (state.isIn(MythBlocks.LAUNCHER_RAIL.get()) && state.get(PoweredRailBlock.POWERED)) {
-            AbstractMinecartEntity minecart = this.getMinecart();
+            AbstractMinecartEntity minecartEntity = this.getMinecart();
+      
+            Vector3d position = minecartEntity.getPositionVec();
+            minecartEntity.setPosition(position.getX(), position.getY() + 0.30D, position.getZ());
 
-            double speedPenalty = minecart.isBeingRidden() ? 0.75D : 1.0D;
-            double maxSpeed = minecart.getMaxSpeedWithRail();
-            Vector3d motion = minecart.getMotion();
+            Vector3d motion = minecartEntity.getMotion();
+            minecartEntity.setMotion(motion.getX(), 1.4D, motion.getZ());
 
-            minecart.move(MoverType.SELF, new Vector3d(MathHelper.clamp(speedPenalty * motion.x, -maxSpeed, maxSpeed), 1.8D, MathHelper.clamp(speedPenalty * motion.z, -maxSpeed, maxSpeed)));
+            this.move(MoverType.SELF, minecartEntity.getMotion());
 
-            if (!this.world.isRemote) {
+            if (!world.isRemote) {
                 ((ServerWorld) world).spawnParticle(ParticleTypes.CLOUD, pos.getX() + 0.5d, pos.getY(), pos.getZ() + 0.5d, 14, 0, 0, 0, 0.2f);
             }
             world.playSound(null, pos, SoundEvents.ENTITY_SNOWBALL_THROW, SoundCategory.NEUTRAL, 0.9f, 0.9f);
+
             callbackInfo.cancel();
         }
     }

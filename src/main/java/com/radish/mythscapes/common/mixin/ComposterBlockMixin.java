bcca71dd03bde50a1b1f228fcee7ef5c2b156a0b
Sarinsa <mikkemusbruker@gmail.com>
2020-11-23 23:11:20 +0100

    Fixed issue with finding valid positions for snails to spawn with composter block.

--- src/main/java/com/radish/mythscapes/common/mixin/ComposterBlockMixin.java
+++ src/main/java/com/radish/mythscapes/common/mixin/ComposterBlockMixin.java
@@ -1,16 +1,12 @@
 package com.radish.mythscapes.common.mixin;
 
-import com.radish.mythscapes.common.core.Mythscapes;
 import com.radish.mythscapes.common.entities.living.SnailEntity;
-import com.radish.mythscapes.common.register.MythEntities;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.ComposterBlock;
-import net.minecraft.entity.SpawnReason;
 import net.minecraft.inventory.ISidedInventoryProvider;
 import net.minecraft.particles.ParticleTypes;
-import net.minecraft.util.Direction;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.server.ServerWorld;
@@ -19,6 +15,9 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Random;
 
 @Mixin(ComposterBlock.class)
@@ -35,23 +34,36 @@ public abstract class ComposterBlockMixin extends Block implements ISidedInvento
 
     @Inject(method = "tick", at = @At("HEAD"))
     public void onTick(BlockState state, ServerWorld worldIn, BlockPos pos, Random rand, CallbackInfo callbackInfo) {
-        Mythscapes.LOGGER.info("Composter ticked!");
         int chanceModifier = state.get(ComposterBlock.LEVEL);
 
-        if (chanceModifier > 0 && rand.nextInt(115) <= chanceModifier) {
-            Mythscapes.LOGGER.info("Composter tryinna spawn snail.");
-            boolean trySpawn = worldIn.getEntitiesWithinAABB(SnailEntity.class, new AxisAlignedBB(pos).expand(15, 15, 15)).size() < 4;
-            BlockPos spawnPos = pos.offset(Direction.byHorizontalIndex(rand.nextInt(4)));
+        if (chanceModifier > 0 && rand.nextInt(55) <= chanceModifier) {
+            boolean tooManySnails = worldIn.getEntitiesWithinAABB(SnailEntity.class, new AxisAlignedBB(pos).grow(25)).size() > 3;
 
-            if (trySpawn && SnailEntity.canSnailSpawn(MythEntities.PYGMY_SNAIL.get(), worldIn, SpawnReason.NATURAL, spawnPos, rand)) {
-                Mythscapes.LOGGER.info("Snail can spawn here!");
-                SnailEntity snailEntity = new SnailEntity(spawnPos.getX(), spawnPos.getY(), spawnPos.getZ(), worldIn);
-                worldIn.addEntity(snailEntity);
+            if (!tooManySnails) {
+                List<BlockPos> spawnPositions = getValidSpawnPositions(pos, worldIn);
 
-                for(int i = 0; i < 8; ++i) {
-                    worldIn.spawnParticle(ParticleTypes.CLOUD, spawnPos.getX() + rand.nextDouble(), spawnPos.getY() + 0.4D, spawnPos.getZ() + rand.nextDouble(), 1, 0.0D, 0.0D, 0.0D, 0);
+                if (!spawnPositions.isEmpty()) {
+                    BlockPos spawnPos = spawnPositions.get(rand.nextInt(spawnPositions.size()));
+                    SnailEntity snailEntity = new SnailEntity(spawnPos.getX() + 0.5D, spawnPos.getY(), spawnPos.getZ() + 0.5D, worldIn);
+                    worldIn.addEntity(snailEntity);
+
+                    for (int i = 0; i < 6; ++i) {
+                        worldIn.spawnParticle(ParticleTypes.CLOUD, snailEntity.getPosX(), snailEntity.getPosY() + (snailEntity.getHeight() / 2), snailEntity.getPosZ(), 1, 0.0D, 0.0D, 0.0D, 0);
+                    }
                 }
             }
         }
+    }
+
+    private static List<BlockPos> getValidSpawnPositions(BlockPos origin, ServerWorld world) {
+        Iterator<BlockPos> scanArea = BlockPos.getAllInBox(new AxisAlignedBB(origin).grow(3, 2, 3)).iterator();
+        List<BlockPos> spawnPositions = new ArrayList<>();
+
+        scanArea.forEachRemaining(blockPos -> {
+            if (!world.getBlockState(blockPos).isSolid() && world.getBlockState(blockPos.down()).isSolid() && blockPos.getY() > 50 && world.canBlockSeeSky(blockPos)) {
+                spawnPositions.add(blockPos.toImmutable());
+            }
+        });
+        return spawnPositions;
     }
 }

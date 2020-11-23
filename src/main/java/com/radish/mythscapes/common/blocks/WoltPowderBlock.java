c84255677feb435b62cc1b7cc2c113160e7522e1
Sarinsa <mikkemusbruker@gmail.com>
2020-11-23 12:29:23 +0100

    Added a whitelist entity type tag for what entities should be considered valid entities to bounce by the wolt powder blocks (this is mainly to prevent certain entities that cannot be applied motion to to just sit on the block and spam sounds and particles without anything happening)

--- src/main/java/com/radish/mythscapes/common/blocks/WoltPowderBlock.java
+++ src/main/java/com/radish/mythscapes/common/blocks/WoltPowderBlock.java
@@ -1,8 +1,13 @@
 package com.radish.mythscapes.common.blocks;
 
+import com.radish.mythscapes.common.tags.MythEntityTags;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.MobEntity;
 import net.minecraft.particles.ParticleTypes;
+import net.minecraft.pathfinding.PathNodeType;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.SoundEvents;
 import net.minecraft.util.math.BlockPos;
@@ -10,6 +15,8 @@ import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
+
+import javax.annotation.Nullable;
 
 public class WoltPowderBlock extends Block {
 
@@ -29,10 +36,13 @@ public class WoltPowderBlock extends Block {
         World entityWorld = entity.getEntityWorld();
         BlockPos pos = entity.getPosition();
 
+        if (!isSwooshableEntity(entity))
+            return;
+
         if (!entityWorld.isRemote) {
             ((ServerWorld) entityWorld).spawnParticle(ParticleTypes.CLOUD, pos.getX() + 0.5d, pos.getY(), pos.getZ() + 0.5d, 14, 0, 0, 0, 0.2f);
         }
-        entityWorld.playSound(entity.getPosX(), entity.getPosY(), entity.getPosZ(), SoundEvents.ENTITY_SNOWBALL_THROW, SoundCategory.NEUTRAL, 0.9f, 0.1f, false);
+        entityWorld.playSound(null, pos, SoundEvents.ENTITY_SNOWBALL_THROW, SoundCategory.NEUTRAL, 0.9f, 0.1f);
 
         Vector3d motion = entity.getMotion();
         double motionY = 1.2D;
@@ -41,5 +51,20 @@ public class WoltPowderBlock extends Block {
             motionY = 1.7D;
         }
         entity.setMotion(new Vector3d(motion.getX(), motionY, motion.getZ()));
+    }
+
+    @Override
+    public PathNodeType getAiPathNodeType(BlockState state, IBlockReader world, BlockPos pos, @Nullable MobEntity entity) {
+        return PathNodeType.DAMAGE_OTHER;
+    }
+
+    /**
+     * Performs a check to see if an entity can/should be
+     * launched upwards from standing on a wolt powder block.
+     *
+     * @param entity - The entity to perform a check on.
+     */
+    private static boolean isSwooshableEntity(Entity entity) {
+        return MythEntityTags.SWOOSH_WHITELIST.contains(entity.getType()) || entity instanceof LivingEntity;
     }
 }

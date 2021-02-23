be8924d5d81337f1aa47d6a7b306ffc584fb99d9
Sarinsa <mikkemusbruker@gmail.com>
2021-02-23 20:56:14 +0100

    Snail is be consume the composter :yes:

--- src/main/java/com/radish/mythscapes/common/entities/living/SnailEntity.java
+++ src/main/java/com/radish/mythscapes/common/entities/living/SnailEntity.java
@@ -7,18 +7,25 @@ import com.radish.mythscapes.common.misc.MythDamageSources;
 import com.radish.mythscapes.common.register.MythItems;
 import com.radish.mythscapes.common.tags.MythBlockTags;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.ComposterBlock;
 import net.minecraft.entity.*;
 import net.minecraft.entity.ai.attributes.AttributeModifierMap;
 import net.minecraft.entity.ai.attributes.Attributes;
 import net.minecraft.entity.ai.goal.LookAtGoal;
+import net.minecraft.entity.ai.goal.MoveToBlockGoal;
 import net.minecraft.entity.ai.goal.WaterAvoidingRandomWalkingGoal;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Food;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.network.datasync.DataParameter;
 import net.minecraft.network.datasync.DataSerializers;
 import net.minecraft.network.datasync.EntityDataManager;
+import net.minecraft.particles.BlockParticleData;
+import net.minecraft.particles.ItemParticleData;
+import net.minecraft.particles.ParticleTypes;
 import net.minecraft.pathfinding.PathNodeType;
 import net.minecraft.stats.Stats;
 import net.minecraft.util.*;
@@ -26,6 +33,7 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.world.*;
 import net.minecraft.world.biome.Biome;
+import net.minecraft.world.server.ServerWorld;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
@@ -33,9 +41,10 @@ import java.util.Map;
 import java.util.Random;
 
 public class SnailEntity extends CreatureEntity {
-    
+
     private static final DataParameter<String> SNAIL_TYPE = EntityDataManager.createKey(SnailEntity.class, DataSerializers.STRING);
     private static final DataParameter<Boolean> FROM_BUCKET = EntityDataManager.createKey(SnailEntity.class, DataSerializers.BOOLEAN);
+    private static final DataParameter<Boolean> HAS_EATEN = EntityDataManager.createKey(SnailEntity.class, DataSerializers.BOOLEAN);
 
     public int timeUntilShellShed = rand.nextInt(8000) + 3000;
 
@@ -46,7 +55,8 @@ public class SnailEntity extends CreatureEntity {
 
     @Override
     public void registerGoals() {
-        this.goalSelector.addGoal(1, new WaterAvoidingRandomWalkingGoal(this, 1.0f));
+        this.goalSelector.addGoal(0, new MoveTowardsComposterGoal<>(this, 1.05F, 15, 70));
+        this.goalSelector.addGoal(1, new WaterAvoidingRandomWalkingGoal(this, 1.0F));
         this.goalSelector.addGoal(2, new LookAtGoal(this, PlayerEntity.class, 10.0F));
     }
 
@@ -57,6 +67,7 @@ public class SnailEntity extends CreatureEntity {
         // statically initialized, so this should cause no problems.
         this.dataManager.register(SNAIL_TYPE, SnailTypeRegister.JUNGLE.getName().toString());
         this.dataManager.register(FROM_BUCKET, false);
+        this.dataManager.register(HAS_EATEN, false);
     }
 
     @Override
@@ -114,6 +125,18 @@ public class SnailEntity extends CreatureEntity {
 
     public boolean isFromBucket() {
         return this.dataManager.get(FROM_BUCKET);
+    }
+
+    /**
+     * @return Whether this snail has eaten from a composter
+     *         or not during it's lifespan.
+     */
+    public boolean hasEaten() {
+        return this.dataManager.get(HAS_EATEN);
+    }
+
+    public void setHasEaten(boolean hasEaten) {
+        this.dataManager.set(HAS_EATEN, hasEaten);
     }
 
     public void setFromBucket(boolean fromBucket) {
@@ -185,6 +208,7 @@ public class SnailEntity extends CreatureEntity {
         super.writeAdditional(compound);
         compound.putString("SnailType", this.getSnailType().getName().toString());
         compound.putBoolean("FromBucket", this.isFromBucket());
+        compound.putBoolean("HasEaten", this.hasEaten());
     }
 
     @Override
@@ -192,6 +216,7 @@ public class SnailEntity extends CreatureEntity {
         String snailType = compound.getString("SnailType");
         this.setSnailType(SnailTypeRegister.getFromName(snailType));
         this.setFromBucket(compound.getBoolean("FromBucket"));
+        this.setHasEaten(compound.getBoolean("HasEaten"));
 
         super.readAdditional(compound);
     }
@@ -201,5 +226,81 @@ public class SnailEntity extends CreatureEntity {
                 .createMutableAttribute(Attributes.FOLLOW_RANGE, 12.0D)
                 .createMutableAttribute(Attributes.MOVEMENT_SPEED, 0.1D)
                 .createMutableAttribute(Attributes.MAX_HEALTH, 3.0D);
+    }
+
+    private static class MoveTowardsComposterGoal<T extends SnailEntity> extends MoveToBlockGoal {
+
+        private final int maxEatTimerCount;
+        private int eatTimer;
+
+        public MoveTowardsComposterGoal(T snailEntity, double speedIn, int length, int maxEatTimerCount) {
+            super(snailEntity, speedIn, length);
+            this.maxEatTimerCount = maxEatTimerCount;
+        }
+
+        protected boolean getIsAboveDestination() {
+            return super.getIsAboveDestination() || this.creature.getPosition().equals(this.destinationBlock);
+        }
+
+        @Override
+        public void tick() {
+            super.tick();
+
+            if (this.getIsAboveDestination()) {
+                --this.eatTimer;
+
+                if (this.eatTimer <= 0) {
+                    CreatureEntity entity = this.creature;
+                    BlockState state = entity.world.getBlockState(this.destinationBlock);
+
+                    if (state.isIn(Blocks.COMPOSTER)) {
+                        entity.world.setBlockState(this.destinationBlock, state.with(ComposterBlock.LEVEL, state.get(ComposterBlock.LEVEL) - 1));
+                    }
+                    ((SnailEntity)entity).setHasEaten(true);
+                }
+                else if (this.eatTimer % 4 == 1) {
+                    this.playEffects();
+                }
+            }
+        }
+
+        private void playEffects() {
+            CreatureEntity entity = this.creature;
+
+            entity.world.playSound(null, entity.getPosition(), SoundEvents.ENTITY_GENERIC_EAT, SoundCategory.NEUTRAL, 0.6F, (entity.getRNG().nextFloat() - entity.getRNG().nextFloat()) * 0.2F + 1.2F);
+
+            // Maybe do this, maybe not, we'll see
+            /*
+            if (!entity.world.isRemote) {
+                ServerWorld serverWorld = (ServerWorld) entity.world;
+                Vector3d vector3d = (new Vector3d(((double)entity.getRNG().nextFloat() - 0.5D) * 0.1D, Math.random() * 0.1D + 0.1D, 0.0D)).rotatePitch(-entity.rotationPitch * ((float)Math.PI / 180F)).rotateYaw(-entity.rotationYaw * ((float)Math.PI / 180F));
+                serverWorld.addParticle(new BlockParticleData(ParticleTypes.BLOCK, Blocks.PODZOL.getDefaultState()).setPos(this.destinationBlock),
+                        entity.getPosX() + entity.getLookVec().x / 2.0D,
+                        entity.getPosY(),
+                        entity.getPosZ() + entity.getLookVec().z / 2.0D,
+                        vector3d.x,
+                        vector3d.y + 0.05D,
+                        vector3d.z);
+            }
+
+             */
+        }
+
+        @Override
+        protected boolean shouldMoveTo(IWorldReader world, BlockPos pos) {
+            if (((SnailEntity)this.creature).hasEaten())
+                return false;
+
+            if (world.isAirBlock(pos.up()) && (world.getBlockState(pos).isIn(Blocks.COMPOSTER))) {
+                return world.getBlockState(pos).get(ComposterBlock.LEVEL) > 0;
+            }
+            return false;
+        }
+
+        @Override
+        public void startExecuting() {
+            super.startExecuting();
+            this.eatTimer = maxEatTimerCount;
+        }
     }
 }

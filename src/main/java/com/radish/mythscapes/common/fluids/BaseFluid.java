06a7930be6c99ac8a4355bab735d4a6f91c5dc64
Sarinsa <mikkemusbruker@gmail.com>
2021-02-27 15:40:05 +0100

    Cleaned up BaseFluid a bit.

--- src/main/java/com/radish/mythscapes/common/fluids/BaseFluid.java
+++ src/main/java/com/radish/mythscapes/common/fluids/BaseFluid.java
@@ -1,9 +1,8 @@
 package com.radish.mythscapes.common.fluids;
 
-import net.minecraft.fluid.Fluid;
 import net.minecraft.fluid.FluidState;
 import net.minecraft.item.Item;
-import net.minecraft.state.StateContainer;
+import net.minecraftforge.client.IWeatherParticleRenderHandler;
 import net.minecraftforge.fluids.ForgeFlowingFluid;
 import org.jetbrains.annotations.Nullable;
 
@@ -20,8 +19,7 @@ public class BaseFluid extends ForgeFlowingFluid {
     }
 
     public BaseFluid(Properties properties) {
-        super(properties);
-        this.bottleItem = null;
+        this(null, properties);
     }
 
     @Nullable
@@ -37,49 +35,5 @@ public class BaseFluid extends ForgeFlowingFluid {
     @Override
     public int getLevel(FluidState state) {
         return 0;
-    }
-
-    public static class Flowing extends BaseFluid {
-
-        public Flowing(Supplier<Item> bottleItem, Properties properties) {
-            super(bottleItem, properties);
-        }
-
-        public Flowing(Properties properties) {
-            super(properties);
-            setDefaultState(getStateContainer().getBaseState().with(LEVEL_1_8, 7));
-        }
-
-        protected void fillStateContainer(StateContainer.Builder<Fluid, FluidState> builder) {
-            super.fillStateContainer(builder);
-            builder.add(LEVEL_1_8);
-        }
-
-        public int getLevel(FluidState state) {
-            return state.get(LEVEL_1_8);
-        }
-
-        public boolean isSource(FluidState state) {
-            return false;
-        }
-    }
-
-    public static class Source extends BaseFluid {
-
-        public Source(Supplier<Item> bottleItem, Properties properties) {
-            super(bottleItem, properties);
-        }
-
-        public Source(Properties properties) {
-            super(properties);
-        }
-
-        public int getLevel(FluidState state) {
-            return 8;
-        }
-
-        public boolean isSource(FluidState state) {
-            return true;
-        }
     }
 }

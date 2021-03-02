c8f49e5d531fd26b0a1ba2ecfaafb0768b183e75
Sarinsa <mikkemusbruker@gmail.com>
2021-03-02 21:59:38 +0100

    Added incredibly mega important package-info.java

---
+++ src/main/java/com/radish/mythscapes/common/blocks/compat/package-info.java
@@ -0,0 +1,7 @@
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+package com.radish.mythscapes.common.blocks.compat;
+
+import mcp.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
